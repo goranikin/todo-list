618cbf3e1efee40affe452d3d04a0f2450d8861a
goranikin <goranikin@snu.ac.kr>
2024-09-27 15:42:57 +0900

    make layout and fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import './reset.css';
 
-import { useReducer } from 'react';
+import { PostItem } from './PostResponse.tsx';
 
 export const App = () => {
-  const [count, increment] = useReducer((c: number) => c + 1, 0);
-
-  return <button onClick={increment}>{count}</button>;
+  return (
+    <div>
+      <div className="container">
+        <div className="left-wrapper">
+          <h2>포스트 목록</h2>
+          <ol>
+            {PostItem({ id: 1 })}
+            {PostItem({ id: 2 })}
+            {PostItem({ id: 3 })}
+          </ol>
+        </div>
+        <div className="right-wrapper">
+          <h2>내용</h2>
+          <h2>댓글</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
