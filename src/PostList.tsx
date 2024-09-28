6bd35766b0fc7d35fb7995c7d74ce76c7e256aa6
goranikin <goranikin@snu.ac.kr>
2024-09-28 23:05:01 +0900

    마무리!

--- src/PostList.tsx
+++ src/PostList.tsx
@@ -8,24 +8,30 @@ interface PostListProps {
   selectedRef: React.RefObject<HTMLButtonElement>;
 }
 
-export const PostList: React.FC<PostListProps> = ({ selectedPostId, setSelectedPostId, selectedRef }) => {
+export const PostList: React.FC<PostListProps> = ({
+  selectedPostId,
+  setSelectedPostId,
+  selectedRef,
+}) => {
   return (
     <>
       <h2 className="gloria-hallelujah-regular">Post Index</h2>
-  {Array.from({ length: 100 }, (_, index: number) => {
-    const id = (index + 1).toString();
-    return (
-      <button
-        key={id}
-    ref={selectedPostId === id ? selectedRef : null}
-    className={`title cursor-pointer indie-flower-regular ${selectedPostId === id ? 'selected' : ''}`}
-    onClick={() => { setSelectedPostId(id); }}
-  >
-    <span>{id}. </span>
-    <span>{PostTitleItem({ id })}</span>
-    </button>
+      {Array.from({ length: 100 }, (_, index: number) => {
+        const id = (index + 1).toString();
+        return (
+          <button
+            key={id}
+            ref={selectedPostId === id ? selectedRef : null}
+            className={`title cursor-pointer indie-flower-regular ${selectedPostId === id ? 'selected' : ''}`}
+            onClick={() => {
+              setSelectedPostId(id);
+            }}
+          >
+            <span>{id}. </span>
+            <span>{PostTitleItem({ id })}</span>
+          </button>
+        );
+      })}
+    </>
   );
-  })}
-  </>
-);
 };
